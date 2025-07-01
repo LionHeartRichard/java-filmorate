6c8821b8cdeb9377a29e529813f46748808389be
LionHeartRichard <leonhearthrich@gmail.com>
2025-07-01 12:11:24 +0300

    Refactoring data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,3 @@
-INSERT INTO film (name,description,release_date,duration, mpa_id) VALUES 
-('star wars','Epic, legendary films','1977-05-25',121, 1),
-('name-film','description-film','1977-05-25',90, 2),
-('D hunter vampire','D hunter vampire','2000-01-01',102, 3);
-
-INSERT INTO person (email,login,name,birthday) VALUES 
-('dolores@gmail.com','loginDolores','Dolores','1992-04-27'),
-('gmail@com','loginPerson','namePerson','2000-01-01'),
-('j7@com','loginJ-7','Name j7','1990-09-19');
-
 INSERT INTO genre (name) VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
 
 INSERT INTO mpa (name) VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
