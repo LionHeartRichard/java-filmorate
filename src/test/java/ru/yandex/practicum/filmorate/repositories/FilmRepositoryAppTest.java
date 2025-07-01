67372b56001a641b0d72a2438ba1ea5e3f624f34
LionHeartRichard <leonhearthrich@gmail.com>
2025-07-01 12:31:23 +0300

    Refactoring APP tests / delete import

--- src/test/java/ru/yandex/practicum/filmorate/repositories/FilmRepositoryAppTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/repositories/FilmRepositoryAppTest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.repositories;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
--- src/test/java/ru/yandex/practicum/filmorate/repositories/LikeRepositoryAppTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/repositories/LikeRepositoryAppTest.java
@@ -1,12 +1,6 @@
 package ru.yandex.practicum.filmorate.repositories;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-import java.util.Optional;
-
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
