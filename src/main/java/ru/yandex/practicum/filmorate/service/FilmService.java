81ff4d2ba8325452a4aeb0183f4c1330e3a569c6
LionHeartRichard <leonhearthrich@gmail.com>
2025-07-14 04:07:29 +0300

    CheckStyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.filmorate.service;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
