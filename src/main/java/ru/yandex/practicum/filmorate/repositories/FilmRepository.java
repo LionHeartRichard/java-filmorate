bad933b07af0cd98e0484d6ace48fe6aabd1e8dd
Alexander Dobrolyubov <dobrolyubov92@yandex.ru>
2025-07-18 09:20:53 +0300

    Удалил неиспользуемый импорт.

--- src/main/java/ru/yandex/practicum/filmorate/repositories/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repositories/FilmRepository.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.model.Director;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.*;
-import java.util.stream.IntStream;
 
 @Repository
 public class FilmRepository extends BaseRepository<Film> {
